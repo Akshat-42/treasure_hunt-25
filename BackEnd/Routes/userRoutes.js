10fd70dbbf103fdc215412f0328dcc91cf29285d
Vedant-Droid <vedanth290106@gmail.com>
2025-07-29 17:37:18 +0530

    2

--- BackEnd/Routes/userRoutes.js
+++ BackEnd/Routes/userRoutes.js
@@ -1,5 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+// Sample route
+router.get("/", (req, res) => {
+  res.json({ message: "User route is working!" });
+});
 
-export default router;
+module.exports = router;
