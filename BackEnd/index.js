f0869c79d81b950133e2d466a5ab900cad093d89
Vedant-Droid <vedanth290106@gmail.com>
2025-07-29 17:26:39 +0530

    nun

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,11 +1,14 @@
-// index.js
+import userRoutes from "./Routes/userRoutes"
+import cors from "cors"
+
 
 const express = require('express');
 const app = express();
-const PORT = process.env.port;
+const PORT = process.env.port||3000;
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(cors({ origin: `http://localhost:${process.env.frontEndPort}`, credentials: true }));
 
 // Sample route
 app.get('/', (req, res) => {
@@ -14,5 +17,8 @@ app.get('/', (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on ${process.env.backend_url}`);
 });
+
+
+app.use("/user",userRoutes.js)
