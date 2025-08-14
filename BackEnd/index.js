7e68c90acff1050a9a7830e733fe8904676c34d9
Vedant-Droid <vedanth290106@gmail.com>
2025-08-14 23:50:43 +0530

    api response check 5

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -12,7 +12,7 @@ app.use(cors({
 
 // Homepage route
 app.get("/", (req, res) => {
-  console.log("Server is up i guess api res check 4")
+  console.log("Server is up i guess api res check 5")
     res.send("Welcome to the Treasure Hunt API!");
 });
 
@@ -21,5 +21,5 @@ app.get("/test", (req, res) => {
     res.send("Hello from backend!");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
